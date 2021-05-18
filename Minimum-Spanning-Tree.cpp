43d3e881e7333d5c0cbea556da5770e42f97ab3b
Yuvraj Singh Tomar <thakuryuvraj98@gmail.com>
2021-05-18 15:56:45 +0530

    Update Minimum-Spanning-Tree.cpp

--- Minimum-Spanning-Tree.cpp
+++ Minimum-Spanning-Tree.cpp
@@ -59,20 +59,11 @@ void primMST(int graph[V][V])
   
 int main() 
 { 
-	cout<<"enter size of graph";
-	int n,m;
-	cin>>n>>m;
-	int arr[n][m];
-	for(int i=0;i<n;i++){
-		for(int j=0;j<m;j++){
-			cin>>arr[i][j];
-		}
-	}
-    /*int graph[V][V] = { { 0, 2, 0, 6, 0 }, 
+    int graph[V][V] = { { 0, 2, 0, 6, 0 }, 
                         { 2, 0, 3, 8, 5 }, 
                         { 0, 3, 0, 0, 7 }, 
                         { 6, 8, 0, 0, 9 }, 
-                        { 0, 5, 7, 9, 0 } };*/
+                        { 0, 5, 7, 9, 0 } };
     primMST(arr); 
   
     return 0; 
